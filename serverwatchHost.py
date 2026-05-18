b74ab2e9599cae8de05f0f92eb1050def7d47ace
Ben-Haskew <bhaskew08@gmail.com>
2026-05-18 11:39:29 +1000

    updated host script

--- serverwatchHost.py
+++ serverwatchHost.py
@@ -4,18 +4,20 @@ import time
 import platform
 import serial
 
-# os = platform.system()
-# if os == "linux":
-
-# elif os == "windows":
-#     pass
-# elif os == "darwin"
-#     pass
-
-subprocess.run(['stty', '-F', '/dev/ttyACM0', '115200', 'raw', '-crtscts', '-ixon', '-ixoff']) #port config
-
-fd = os.open('/dev/ttyACM0', os.O_RDWR | os.O_NOCTTY) #open port
-time.sleep(0.5)
-os.write(fd, b'test\n') #test string to send. this will be changed to temp data in later revisions.
-print("Sent!") 
-os.close(fd) #close port
+os = platform.system()
+if os == "linux":
+    subprocess.run(['stty', '-F', '/dev/ttyACM0', '115200', 'raw', '-crtscts', '-ixon', '-ixoff']) #port config
+    fd = os.open('/dev/ttyACM0', os.O_RDWR | os.O_NOCTTY) #open port
+    time.sleep(0.5) #wait
+    os.write(fd, b'test\n') #test string to send. this will be changed to temp data in later revisions.
+    print("Sent!") 
+    os.close(fd) #close port
+elif os == "windows":
+    subprocess.run(['stty', '-F', 'COM3', '115200', 'raw', '-crtscts', '-ixon', '-ixoff'])
+    fd = os.open('COM3', os.O_RDWR | os.O_NOCTTY) #open port
+    time.sleep(0.5) #wait
+    os.write(fd, b'test\n') #test string to send. this will be changed to temp data in later revisions.
+    print("Sent!") 
+    os.close(fd) #close port
+elif os == "darwin":
+    print("Incompatible operating system! Refer to the README")
